b22ce486bc79719b4e8520ca93b5026574334ca6
Travis Osterman <travis@osterman.co>
2017-01-11 12:23:44 -0600

    two column to one column ledgers for each account number formatting

--- pyfinance.py
+++ pyfinance.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
-import pandas as pd
-from tabulate import tabulate
+import sys
+
+# external libraries
+libs=['pandas','tabulate']
+try:
+  import pandas as pd
+  from tabulate import tabulate
+except ImportError:
+  print "Unmet dependencies were found.  Here are the dependencies:"
+  for lib in libs:
+    print " * " + lib
+  sys.exit(1)
 
 class finance():
 
@@ -29,22 +39,68 @@ class finance():
      'account': accounts,
      'balance': balances}).sort_values(by='account'))
 
-  def print_ledger_by_account(self,account):
+  def get_ledger_by_account(self,account):
+    # just transactions for this account
     account_ledger = pd.concat([self.ledger.loc[self.ledger['debit'] == account],self.ledger.loc[self.ledger['credit'] == account]])
-    credit = (account_ledger.loc[account_ledger['credit'] == account])['amount']
-    exchange = (account_ledger.loc[account_ledger['credit'] == account])['exchange']
-    # account_ledger['amount'] = credit = credit.multiply(exchange)
-    # todo fix this
+
+    # separate credits and debits
+    credits = account_ledger.loc[account_ledger.loc[:,'credit'] == account].copy()
+    debits  = account_ledger.loc[account_ledger.loc[:,'debit']  == account].copy()
+
+    # when crediting an account multiply by the exhange rate
+    # when debiting an account, change the sign of the transaction
+    new_credit_amounts = credits['amount'].multiply(account_ledger.loc[account_ledger['credit'] == account]['exchange'])
+    new_debits_amounts =  debits['amount'].multiply(-1)
+
+    # replace the amount column for each frame
+    credits.loc[:,'amount'] = new_credit_amounts
+    debits.loc[:,'amount']  = new_debits_amounts
+
+    # replace two column ledger with single column since it is for a specific account
+    credits['account'] = credits.loc[:,'debit']
+    debits['account']  = debits.loc[:,'credit']
+
+    # drop columns we no longer care about
+    drop = ['debit','credit','exchange']
+    credits = credits.drop(drop, 1)
+    debits  =  debits.drop(drop,1)
+
+    # bring frames together and order by date
+    account_ledger = pd.concat([credits,debits]).sort_values('date')
+
+    # create a running balance
+    account_ledger['balance'] = account_ledger['amount'].cumsum()
+
+    # reorder the columns
+    account_ledger = account_ledger[['date','account','category','description','amount','balance']]
+
+    return account_ledger
+
+  def print_ledger_by_account(self,account):
+
+    account_ledger = self.get_ledger_by_account(account)
+
+    # change format of currency columns
+    cur_columns = ['amount','balance']
+    for col in cur_columns:
+      account_ledger[col] = account_ledger[col].map('{:,.2f}'.format)
+
+    # print account header
+    print "+------------+-----------------+\n| ",account
     self.print_table(account_ledger)
 
-  def print_ledger_all_accounts(self):
+  def get_accounts(self):
     accounts = pd.unique(self.ledger[['debit', 'credit']].values.ravel())
+    return sorted(accounts)
+
+  def print_ledger_all_accounts(self):
+    accounts = self.get_accounts()
+
     for account in accounts:
-      print "\n[[",account,"]]"
       self.print_ledger_by_account(account)
 
   def print_table(self,data):
-    print tabulate(data,showindex=False,headers="keys",tablefmt="grid")
+    print tabulate(data,showindex=False,headers="keys",tablefmt="grid",numalign="right",stralign="right")
 
   def get_number_transactions(self):
     return len(self.ledger.index)
@@ -60,7 +116,10 @@ def main():
          ledger_file = 'ledger.tsv.csv',
          account_file = 'credit.tsv.csv')
   a.all()
+  #a.print_ledger_by_account('td-checking')
+  #a.print_ledger_by_account('vu-checking')
   print "done"
 
 if __name__ == '__main__':
   main()
+  sys.exit()
