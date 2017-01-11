1078779f9cfbf91df7c1c69f641788e36a903f12
Travis Osterman <travis@wsor.net>
2017-01-11 00:02:15 -0600

    improved printing object oriented

--- pyfinance.py
+++ pyfinance.py
@@ -1,40 +1,65 @@
 #!/usr/bin/env python
 
 import pandas as pd
-class account():
-  start_balance = 0
-  limit = 0
+from tabulate import tabulate
 
-  def __init__(self,starting_balance,limit):
-    self.start_balance = starting_balance
-    self.limit = limit
+class finance():
+
+  def __init__(self,ledger_file,account_file):
+    self.ledger = pd.read_csv('ledger.tsv.csv',sep='\t')
+    self.accounts = account_file
+
+  def print_balances(self):
+    balances = []
+    accounts = pd.unique(self.ledger[['debit', 'credit']].values.ravel())
+    for account in accounts:
+      account_ledger = pd.concat([self.ledger.loc[self.ledger['debit'] == account],self.ledger.loc[self.ledger['credit'] == account]])
+      debit  = -1 * (account_ledger.loc[account_ledger['debit']  == account])['amount'].sum()
+      credit = (account_ledger.loc[account_ledger['credit'] == account])['amount']
+      exchange = (account_ledger.loc[account_ledger['credit'] == account])['exchange']
+      credit = credit.multiply(exchange).sum()
+      balance = debit + credit
+      balances.append(balance)
+      #print "********************************************************"
+      #print account,':',balance
+      #print "********************************************************"
+      #print account_ledger
+      #print 'exchange',exchange
+    self.print_table(pd.DataFrame({
+     'account': accounts,
+     'balance': balances}).sort_values(by='account'))
+
+  def print_ledger_by_account(self,account):
+    account_ledger = pd.concat([self.ledger.loc[self.ledger['debit'] == account],self.ledger.loc[self.ledger['credit'] == account]])
+    credit = (account_ledger.loc[account_ledger['credit'] == account])['amount']
+    exchange = (account_ledger.loc[account_ledger['credit'] == account])['exchange']
+    # account_ledger['amount'] = credit = credit.multiply(exchange)
+    # todo fix this
+    self.print_table(account_ledger)
+
+  def print_ledger_all_accounts(self):
+    accounts = pd.unique(self.ledger[['debit', 'credit']].values.ravel())
+    for account in accounts:
+      print "\n[[",account,"]]"
+      self.print_ledger_by_account(account)
+
+  def print_table(self,data):
+    print tabulate(data,showindex=False,headers="keys",tablefmt="grid")
+
+  def get_number_transactions(self):
+    return len(self.ledger.index)
+
+  def all(self):
+    print "transactions:",self.get_number_transactions()
+    self.print_ledger_all_accounts()
+    self.print_balances()
 
 def main():
   print "main"
-  ledger = pd.read_csv('ledger.tsv.csv',sep='\t')
-  # print ledger
-
-  balances = []
-  accounts = pd.unique(ledger[['debit', 'credit']].values.ravel())
-  for account in accounts:
-    account_ledger = pd.concat([ledger.loc[ledger['debit'] == account],ledger.loc[ledger['credit'] == account]])
-    debit  = -1 * (account_ledger.loc[account_ledger['debit']  == account])['amount'].sum()
-    credit = (account_ledger.loc[account_ledger['credit'] == account])['amount']
-    exchange = (account_ledger.loc[account_ledger['credit'] == account])['exchange']
-    credit = credit.multiply(exchange).sum()
-    balance = debit + credit
-    balances.append(balance)
-    print "********************************************************"
-    print account,':',balance
-    print "********************************************************"
-    print account_ledger
-    #print 'exchange',exchange
-    
-    print '\n\n'
-  print pd.DataFrame({
-     'account': accounts,
-     'balance': balances}).sort_values(by='account')
-  print "transactions:",len(ledger.index)
+  a = finance(
+         ledger_file = 'ledger.tsv.csv',
+         account_file = 'credit.tsv.csv')
+  a.all()
   print "done"
 
 if __name__ == '__main__':
