c4179212cf637956a842866fabc1c555fc2152fb
Travis Osterman <travis@wsor.net>
2017-01-10 22:56:03 -0600

    basically v1.0

--- pyfinance.py
+++ pyfinance.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
+import pandas as pd
 class account():
   start_balance = 0
+  limit = 0
+
+  def __init__(self,starting_balance,limit):
+    self.start_balance = starting_balance
+    self.limit = limit
 
 def main():
   print "main"
-  account1 = account()
+  ledger = pd.read_csv('ledger.tsv.csv',sep='\t')
+  # print ledger
+
+  balances = []
+  accounts = pd.unique(ledger[['debit', 'credit']].values.ravel())
+  for account in accounts:
+    account_ledger = pd.concat([ledger.loc[ledger['debit'] == account],ledger.loc[ledger['credit'] == account]])
+    debit  = -1 * (account_ledger.loc[account_ledger['debit']  == account])['amount'].sum()
+    credit = (account_ledger.loc[account_ledger['credit'] == account])['amount'].sum()
+    balance = debit + credit
+    balances.append(balance)
+    print account,balance
+    print account_ledger
+    print "********************************************************"
+
+  print balances
+  print accounts
+  print pd.DataFrame({'account': accounts,
+     'balance': balances}).sort_index(by='account')
   print "done"
 
 if __name__ == '__main__':
